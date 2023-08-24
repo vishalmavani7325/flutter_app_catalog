59b083c2bd955eb4417a4081c9c260eb9484eaa8
vishal_mavani <mavaniv2577@gmail.com>
2023-08-24 15:34:12 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_app_catalog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_catalog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_catalog/pages/home_page.dart';
+import 'package:flutter_app_catalog/pages/login_page_demo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: const HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      // initialRoute: "/home",
+
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/login": (context) => const LoginPage(),
+      },
+    );
+  }
+}
